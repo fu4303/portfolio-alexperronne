f5c91d8e623f50ec6d043186c4d144d39487ea83
Alex Perronnet <alexperronnet@outlook.com>
2020-12-13 22:04:06 +0100

    Wrap config files in config folder

---
+++ config/tailwind.js
@@ -0,0 +1,48 @@
+const theme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem"
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", ...theme.fontFamily.sans]
+      },
+      colors: {
+        success: "var(--color-success)",
+        warning: "var(--color-warning)",
+        danger: "var(--color-danger)"
+      },
+      backgroundColor: {
+        primary: "var(--bg-primary)",
+        secondary: "var(--bg-secondary)",
+        tertiary: "var(--color-primary)"
+      },
+      textColor: {
+        primary: "var(--text-primary)",
+        secondary: "var(--text-secondary)",
+        tertiary: "var(--color-primary)"
+      },
+      borderColor: {
+        primary: "var(--bg-secondary)",
+        secondary: "var(--bg-primary)",
+        tertiary: "var(--color-primary)"
+      },
+      divideColor: {
+        primary: "var(--bg-secondary)",
+        secondary: "var(--bg-primary)",
+        tertiary: "var(--color-primary)"
+      },
+      fill: {
+        primary: "var(--bg-secondary)",
+        secondary: "var(--bg-primary)",
+        tertiary: "var(--color-primary)"
+      },
+      spacing: {
+        84: "21rem"
+      }
+    }
+  }
+}
---
+++ config/website.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "Alex Perronnet",
+  author: "@alexperronnet",
+  url: "https://alexperronnet.io",
+  gaTrackingId: "UA-156157580-2",
+  description:
+    "I am Alex Perronnet, a french freelance developer and designer. I am also an open-source contributor and a content creator"
+}
