47bece53f69aece6d1c4c3ed35e7b418bd5d6396
Alex Perronnet <alexperronnet@outlook.com>
2021-03-25 10:44:26 +0100

    Udpade contact.js
    Switch <alex@alexperronnet.io> to <me@alexperronnet.io>

--- src/sections/contact.js
+++ src/sections/contact.js
@@ -26,13 +26,13 @@ export const Contact = () => {
         I am open to lots of new opportunities, freelance or a fulltime
         position. You can contact me by trying to send a homing pigeon but
         honestly the best is still to send me an {""}
-        <StyledLink href="mailto:alex@alexperronnet.io" onMouseEnter={playPop}>
+        <StyledLink href="mailto:me@alexperronnet.io" onMouseEnter={playPop}>
           email
         </StyledLink>
         .
       </p>
       <FlexCopy>
-        <CopyBox value="alex@alexperronnet.io" />
+        <CopyBox value="me@alexperronnet.io" />
         <CopyBox value="@alexperronnet" />
       </FlexCopy>
       <CustomTooltip title="BTW it's better than scroll" placement="top">
